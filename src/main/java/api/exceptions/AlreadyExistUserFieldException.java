c6e836ec282edc57a7cb353dd35a7ec1b1abde1f
jbernal <jbernal@etsisi.upm.es>
2017-01-25 08:43:51 +0100

    View Refactoring

--- src/main/java/api/exceptions/AlreadyExistUserFieldException.java
+++ src/main/java/api/exceptions/AlreadyExistUserFieldException.java
@@ -4,7 +4,7 @@ public class AlreadyExistUserFieldException extends ApiException {
 
     private static final long serialVersionUID = -1344640670884805385L;
 
-    public static final String DESCRIPTION = "Campo username o email ya existente en la BD";
+    public static final String DESCRIPTION = "Campo mobile o email ya existente en la BD";
 
     public static final int CODE = 1;
 
