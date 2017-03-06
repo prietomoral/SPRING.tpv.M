65c279fe34415c37336c0e1e3db7ed60810182ee
JRSP <JRSP@Javi-AsusA555L>
2017-03-06 21:39:28 +0100

    Create a consult for search sold products in a period of time

--- src/main/java/daos/core/ShoppingDao.java
+++ src/main/java/daos/core/ShoppingDao.java
@@ -6,5 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import entities.core.Shopping;
 
 public interface ShoppingDao extends JpaRepository<Shopping, Long>{
-    
+    /*
+     * 
+     * select description,amount, productId,Ticket_id,created from tpv.shopping as s join tpv.ticket_shopping as ts 
+on s.id=ts.shoppingList_id 
+join tpv.ticket as t on ts.Ticket_id=t.id
+where created BETWEEN '2017-03-06 00:00:00' and '2017-03-06 21:13:01'*/
 }
