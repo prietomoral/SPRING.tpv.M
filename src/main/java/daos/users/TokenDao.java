c3b63c7879c2410e6088671047d3cb12efa7e5d9
JorgeCoke <cokelas@gmail.com>
2017-03-08 13:02:33 +0100

    Feature#13.3 done - TokenDao:deleteByCreationDateLessThan()

--- src/main/java/daos/users/TokenDao.java
+++ src/main/java/daos/users/TokenDao.java
@@ -1,11 +1,17 @@
 package daos.users;
 
+import java.util.Date;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import entities.users.Token;
 import entities.users.User;
 
+@Transactional
 public interface TokenDao extends JpaRepository<Token, Integer> {
 
     Token findByUser(User user);
+    int deleteByCreationDateLessThan(Date ahoraMenosTiempoExpiracion);
+    
 }
