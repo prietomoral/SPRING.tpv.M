344733d357528dd5635e67375d618c50eec2d22f
Sergio Banegas <sbanegas92@gmail.com>
2017-03-09 10:00:25 +0100

    Dao created

--- src/main/java/daos/core/ArticleDao.java
+++ src/main/java/daos/core/ArticleDao.java
@@ -2,7 +2,10 @@ package daos.core;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import entities.core.Article;
 
@@ -10,4 +13,8 @@ public interface ArticleDao extends JpaRepository<Article, Long> {
     public List<Article> findAll();
 
     public Article findById(long id);
+
+    // TODO cambiar la query para admitir parámetros de búsqueda
+    @Query("SELECT a FROM Article a")
+    public Page<Article> search(Pageable pageable);
 }
