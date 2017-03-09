8e32e0571cc823a89a4d16454beefc367558d997
JRSP <JRSP@Javi-AsusA555L>
2017-03-09 10:21:00 +0100

    Refactoring StatisticService

--- src/main/java/services/StatisticService.java
+++ src/main/java/services/StatisticService.java
@@ -1,5 +1,20 @@
 package services;
 
-public class StatisticService {
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+import org.springframework.stereotype.Service;
+
+@Service
+public class StatisticService {
+/*consulta sql:
+    select s.description, sum(s.amount), s.productId from tpv.shopping as s join tpv.ticket_shopping as ts 
+    on s.id=ts.shoppingList_id 
+    join tpv.ticket as t on ts.Ticket_id=t.id
+    group by productId*/
+    public void getSoldProductsStatistic(){
+        String jpqlConsult="Select s.productId, s.description, SUM(s.amount)";
+        //EntityManagerFactory em=Persistence.createEntityManagerFactory("JPA_Factoria"); 
+        
+    }
 }
