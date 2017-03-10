5bbb884b3b1d29ff6edfff229044d17d30606e45
Jahir Lozano <jahirlozano@gmail.com>
2017-03-10 20:17:26 +0100

    Create Embroideries

--- src/main/webapp/app/config.js
+++ src/main/webapp/app/config.js
@@ -36,6 +36,11 @@ tpv.config(function ($routeProvider) {
             controller: "EmbroideryController",
             controllerAs: "vm"
         })
+        .when("/feature03/new-embroidery", {
+            templateUrl: "app/components/feature03/newEmbroidery.html",
+            controller: "addEmbroideryController",
+            controllerAs: "vm"
+        })
         .when("/feature03/list-textile-printing", {
             templateUrl: "app/components/feature03/indexTextilePrinting.html",
             controller: "TextilePrintinController",
