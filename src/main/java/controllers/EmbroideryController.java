d4b42ba3e536420ab10356dbcf089a92847bb764
Sergio Banegas <sbanegas92@gmail.com>
2017-03-10 11:57:50 +0100

    Search embroideries controller function created

--- src/main/java/controllers/EmbroideryController.java
+++ src/main/java/controllers/EmbroideryController.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 
 import daos.core.EmbroideryDao;
@@ -14,18 +17,18 @@ import wrappers.EmbroideryWrapper;
 public class EmbroideryController {
 
     private EmbroideryDao embroideryDao;
-    
+
     @Autowired
     public void setEmbroideryDao(EmbroideryDao embroideryDao) {
         this.embroideryDao = embroideryDao;
     }
 
     public List<EmbroideryWrapper> allEmbroidery() {
-        
+
         List<EmbroideryWrapper> embroideryList = new ArrayList<EmbroideryWrapper>();
-        
+
         for (Embroidery embroidery : embroideryDao.findAll()) {
-            
+
             EmbroideryWrapper embroideryWrapper = new EmbroideryWrapper();
 
             embroideryWrapper.setReference(embroidery.getReference());
@@ -39,5 +42,17 @@ public class EmbroideryController {
         }
         return embroideryList;
 
+    }
+
+    public Page<EmbroideryWrapper> search(Pageable pageable) {
+        Page<Embroidery> page = embroideryDao.search(pageable);
+        List<EmbroideryWrapper> embroideryWrapperList = new ArrayList<>();
+        for (Embroidery embroidery : page.getContent()) {
+            EmbroideryWrapper articleWrapper = new EmbroideryWrapper(embroidery.getId(), embroidery.getReference(),
+                    embroidery.getDescription(), embroidery.getRetailPrice(), embroidery.getStitches(), embroidery.getColors(),
+                    embroidery.getSquareMillimeters());
+            embroideryWrapperList.add(articleWrapper);
+        }
+        return new PageImpl<EmbroideryWrapper>(embroideryWrapperList, pageable, page.getTotalElements());
     }
 }
