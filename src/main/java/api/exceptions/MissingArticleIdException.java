a4d18acd659905b51469e9558574543e331f5130
Fabricio Flores <fabrifloresg@gmail.com>
2017-03-10 16:35:17 +0100

    Add POST for /alerts

---
+++ src/main/java/api/exceptions/MissingArticleIdException.java
@@ -0,0 +1,17 @@
+package api.exceptions;
+
+public class MissingArticleIdException extends ApiException {
+    private static final long serialVersionUID = -1344640670884805385L;
+
+    public static final String DESCRIPTION = "Falta id del artículo";
+
+    public static final int CODE = 1;
+
+    public MissingArticleIdException() {
+        this("");
+    }
+
+    public MissingArticleIdException(String detail) {
+        super(DESCRIPTION + ". " + detail, CODE);
+    }
+}
