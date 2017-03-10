06fef7798f7ae2c023d87ce4018c1cee0616d151
Sergio Banegas <sbanegas92@gmail.com>
2017-03-10 11:34:21 +0100

    Embroidery search added to EmbroideryDao

--- src/main/java/daos/core/EmbroideryDao.java
+++ src/main/java/daos/core/EmbroideryDao.java
@@ -1,12 +1,15 @@
 package daos.core;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import entities.core.Embroidery;
 
 public interface EmbroideryDao extends JpaRepository<Embroidery, Long> {
 
-
+    // TODO cambiar la query para admitir parámetros de búsqueda
+    @Query("SELECT e FROM Embroidery e")
+    public Page<Embroidery> search(Pageable pageable);
 }
