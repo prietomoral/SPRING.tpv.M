8c10f7943f8f4889d642e4dcdb8facac2f503136
Sergio Banegas <sbanegas92@gmail.com>
2017-03-10 12:30:32 +0100

    TextilePrinting search added to TextilePrintingDao

--- src/main/java/daos/core/TextilePrintingDao.java
+++ src/main/java/daos/core/TextilePrintingDao.java
@@ -1,11 +1,15 @@
 package daos.core;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import entities.core.TextilePrinting;
 
 public interface TextilePrintingDao extends JpaRepository<TextilePrinting, Long> {
 
+    // TODO cambiar la query para admitir parámetros de búsqueda
+    @Query("SELECT t FROM TextilePrinting t")
+    public Page<TextilePrinting> search(Pageable pageable);
 }
