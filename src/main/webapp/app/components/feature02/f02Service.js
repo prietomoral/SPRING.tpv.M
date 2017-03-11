991ef5632a816ded119c841f0c75a985bafe8f0a
Tania Guzman <taguzga@gmail.com>
2017-03-11 17:51:21 +0100

    Add form for create providers,method in controller and method in service

--- src/main/webapp/app/components/feature02/f02Service.js
+++ src/main/webapp/app/components/feature02/f02Service.js
@@ -1,5 +1,23 @@
 tpv.service('f02Service', ['$http', '$q', function ($http, $q) {
-   "use strict";
+	"use strict";
    
-   const urlBase="http://localhost:8080/SPRING.tpv.M.1.2.0-SNAPSHOT/api/v0";
+   	const urlBase="http://localhost:8080/SPRING.tpv.M.1.2.0-SNAPSHOT/api/v0";
+   
+   	var feature02Service = {
+   		createProvider:createProvider
+   	};
+   
+   	return feature02Service;
+   
+   	function createProvider(provider) {
+   		return $http({
+   			method: 'POST',
+   			url: urlBase + '/providers',
+   			data:{'id': provider.id, 'company': provider.company , 'address': provider.address, 'mobile':provider.mobile, 'phone':provider.phone, 'paymentConditions':provider.paymentConditions, 'note':provider.note}
+   		}).then(function successCallback(response) {
+	        return response.data;
+   		}, function errorCallback(response) {
+	        return $q.reject(response);
+   		});
+	}
 }]);
