8fea4f7c5b6defcc5c47f226685275315c5e1542
JRSP <JRSP@Javi-AsusA555L>
2017-03-11 21:20:09 +0100

    Crate method in class

--- src/main/java/controllers/StatisticController.java
+++ src/main/java/controllers/StatisticController.java
@@ -1,10 +1,15 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import daos.core.ShoppingDao;
+import services.SemiWrapperStatisticSold;
 import services.StatisticService;
+import wrappers.TotalSoldProductWrapper;
 
 @Controller
 public class StatisticController {
@@ -21,6 +26,16 @@ public class StatisticController {
     @Autowired
     public void setStatisticService(StatisticService statisticService) {
         this.statisticService = statisticService;
+    }
+    
+    public List<TotalSoldProductWrapper> getMostSoldProducts(){
+        List<SemiWrapperStatisticSold> semiWrapperMostSoldProducts=statisticService.mostSoldProductsMapped();
+        List<TotalSoldProductWrapper> mostSoldProducts=new ArrayList<TotalSoldProductWrapper>();
+        for (SemiWrapperStatisticSold sWMSP : semiWrapperMostSoldProducts) {
+            TotalSoldProductWrapper soldProduct=new TotalSoldProductWrapper(sWMSP.getIdProduct(),sWMSP.getTotalAmountSold(),sWMSP.getDescription());
+            mostSoldProducts.add(soldProduct);
+        }
+        return mostSoldProducts;
     }
 
 }
