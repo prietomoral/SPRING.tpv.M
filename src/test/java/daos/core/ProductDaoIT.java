3e5ba9218795de2e55776e45f399d82bd4e3accb
Sergio Banegas <sbanegas92@gmail.com>
2017-03-12 18:07:02 +0100

    Embroidery search function IT completed

--- src/test/java/daos/core/ProductDaoIT.java
+++ src/test/java/daos/core/ProductDaoIT.java
@@ -103,14 +103,30 @@ public class ProductDaoIT {
     }
 
     @Test
-    public void testSearchEmbroideries() {
-        Page<Embroidery> embroideryPage = embroideryDao.search(new PageRequest(1, 2));
+    public void testSearchEmbroideriesWithoutParameters() {
+        Page<Embroidery> embroideryPage = embroideryDao.search(new PageRequest(1, 2), null, null, null, null, null, null, null, null, null,
+                null);
         assertNotNull(embroideryPage);
         assertTrue(embroideryPage.getNumberOfElements() > 0);
         assertEquals(4, embroideryPage.getTotalElements());
         assertEquals(1, embroideryPage.getNumber());
         assertTrue(embroideryPage.hasContent());
         assertEquals("embroidery2", embroideryPage.getContent().get(0).getReference());
+        assertEquals(2, embroideryPage.getTotalPages());
+        assertTrue(embroideryPage.isLast());
+        assertFalse(embroideryPage.isFirst());
+    }
+
+    @Test
+    public void testSearchEmbroideriesWithParameters() {
+        Page<Embroidery> embroideryPage = embroideryDao.search(new PageRequest(1, 2), null, "embroidery", new BigDecimal(20),
+                new BigDecimal(23), 1000, 4000, null, null, 0, 400);
+        assertNotNull(embroideryPage);
+        assertTrue(embroideryPage.getNumberOfElements() > 0);
+        assertEquals(3, embroideryPage.getTotalElements());
+        assertEquals(1, embroideryPage.getNumber());
+        assertTrue(embroideryPage.hasContent());
+        assertEquals("embroidery3", embroideryPage.getContent().get(0).getReference());
         assertEquals(2, embroideryPage.getTotalPages());
         assertTrue(embroideryPage.isLast());
         assertFalse(embroideryPage.isFirst());
