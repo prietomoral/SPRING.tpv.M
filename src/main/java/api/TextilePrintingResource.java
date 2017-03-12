023e6272d2df00f753af83f7308cad6aa750adaa
Sergio Banegas <sbanegas92@gmail.com>
2017-03-12 18:46:22 +0100

    Advanced search added to TextilePrintingResource search function

--- src/main/java/api/TextilePrintingResource.java
+++ src/main/java/api/TextilePrintingResource.java
@@ -1,5 +1,6 @@
 package api;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import api.exceptions.AlreadyExistTextilePrintingException;
@@ -19,38 +21,40 @@ import wrappers.TextilePrintingWrapper;
 @RequestMapping(Uris.VERSION + Uris.TEXTILE_PRINTINGS)
 public class TextilePrintingResource {
 
-	private TextilePrintingController textilePrintingController;
+    private TextilePrintingController textilePrintingController;
 
-	@Autowired
-	public void setTextilePrintingController(TextilePrintingController textilePrintingController) {
-		this.textilePrintingController = textilePrintingController;
-	}
+    @Autowired
+    public void setTextilePrintingController(TextilePrintingController textilePrintingController) {
+        this.textilePrintingController = textilePrintingController;
+    }
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<TextilePrintingWrapper> all() {
-		return this.textilePrintingController.all();
-	}
+    @RequestMapping(method = RequestMethod.GET)
+    public List<TextilePrintingWrapper> all() {
+        return this.textilePrintingController.all();
+    }
 
-	@RequestMapping(method = RequestMethod.POST)
-	public void add(@RequestBody TextilePrintingWrapper textilePrintingWrapper)
-			throws AlreadyExistTextilePrintingException {
-		this.textilePrintingController.add(textilePrintingWrapper);
-	}
+    @RequestMapping(method = RequestMethod.POST)
+    public void add(@RequestBody TextilePrintingWrapper textilePrintingWrapper) throws AlreadyExistTextilePrintingException {
+        this.textilePrintingController.add(textilePrintingWrapper);
+    }
 
-	@RequestMapping(value = Uris.SEARCH, method = RequestMethod.GET)
-	public Page<TextilePrintingWrapper> search(Pageable pageable) {
-		Page<TextilePrintingWrapper> page = textilePrintingController.search(pageable);
-		return page;
-	}
+    @RequestMapping(value = Uris.SEARCH, method = RequestMethod.GET)
+    public Page<TextilePrintingWrapper> search(Pageable pageable, @RequestParam(required = false) String reference,
+            @RequestParam(required = false) String description, @RequestParam(required = false) BigDecimal minRetailPrice,
+            @RequestParam(required = false) BigDecimal maxRetailPrice, @RequestParam(required = false) String type) {
+        Page<TextilePrintingWrapper> page = textilePrintingController.search(pageable, reference, description, minRetailPrice,
+                maxRetailPrice, type);
+        return page;
+    }
 
-	@RequestMapping(value = Uris.ID, method = RequestMethod.DELETE)
-	public void delete(@PathVariable(value = "id") int id) {
-		this.textilePrintingController.delete(id);
-	}
-	
-	@RequestMapping(method = RequestMethod.PUT)
-	public void edit(int id, @RequestBody TextilePrintingWrapper textilePrintingWrapper) {
-		this.textilePrintingController.edit(id,textilePrintingWrapper);
-	}
+    @RequestMapping(value = Uris.ID, method = RequestMethod.DELETE)
+    public void delete(@PathVariable(value = "id") int id) {
+        this.textilePrintingController.delete(id);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public void edit(int id, @RequestBody TextilePrintingWrapper textilePrintingWrapper) {
+        this.textilePrintingController.edit(id, textilePrintingWrapper);
+    }
 
 }
