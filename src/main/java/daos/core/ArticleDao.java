798f1c28a515a4f40e3ec445f069b72586e21051
Sergio Banegas <sbanegas92@gmail.com>
2017-03-12 13:34:49 +0100

    Search function in ArticleDao completed

--- src/main/java/daos/core/ArticleDao.java
+++ src/main/java/daos/core/ArticleDao.java
@@ -1,9 +1,12 @@
 package daos.core;
 
+import java.math.BigDecimal;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import entities.core.Article;
 
@@ -11,7 +14,12 @@ public interface ArticleDao extends JpaRepository<Article, Long> {
 
     public Article findById(long id);
 
-    // TODO cambiar la query para admitir parámetros de búsqueda
-    @Query("SELECT a FROM Article a")
-    public Page<Article> search(Pageable pageable);
+    @Query("SELECT a FROM Article a WHERE (:reference IS NULL OR a.reference = :reference) "
+            + "AND (:description IS NULL OR a.description LIKE CONCAT('%',:description,'%')) "
+            + "AND (:minRetailPrice IS NULL OR a.retailPrice >= :minRetailPrice) "
+            + "AND (:maxRetailPrice IS NULL OR a.retailPrice <= :maxRetailPrice) "
+            + "AND (:onlyOnStock IS NULL OR ( :onlyOnStock = FALSE OR a.stock > 0))")
+    public Page<Article> search(Pageable pageable, @Param("reference") String reference, @Param("description") String description,
+            @Param("minRetailPrice") BigDecimal minRetailPrice, @Param("maxRetailPrice") BigDecimal maxRetailPrice,
+            @Param("onlyOnStock") boolean onlyOnStock);
 }
