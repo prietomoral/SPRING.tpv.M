d989ab8f81008e48f489d57cbbb12b2e9f36521d
Sergio Banegas <sbanegas92@gmail.com>
2017-03-12 11:57:11 +0100

    Implemented HTTP Call instead static array

--- src/main/webapp/app/components/feature04/f04Service.js
+++ src/main/webapp/app/components/feature04/f04Service.js
@@ -3,37 +3,6 @@ tpv.service('f04Service', function ($http, $q) {
    
    	const urlBase = "http://localhost:8080/SPRING.tpv.M.1.2.0-SNAPSHOT/api/v0";
 	
-	var textilePrintings = [
-		{
-			"id": 9,
-			"reference": "REFERENCE9",
-			"description": "DESCRIPTION 9",
-			"retailPrice": 666.9,
-			"type": "Type 9"
-		},
-		{
-			"id": 10,
-			"reference": "REFERENCE10",
-			"description": "DESCRIPTION 10",
-			"retailPrice": 666.10,
-			"type": "Type 10"
-		},
-		{
-			"id": 11,
-			"reference": "REFERENCE11",
-			"description": "DESCRIPTION 11",
-			"retailPrice": 666.11,
-			"type": "Type 11"
-		},
-		{
-			"id": 12,
-			"reference": "REFERENCE12",
-			"description": "DESCRIPTION 12",
-			"retailPrice": 666.12,
-			"type": "Type 12"
-		}
-	];
-	
 	this.request = (config) => {
 	      let deferred = $q.defer();
 	      $http(config).then((response) => {
@@ -67,8 +36,13 @@ tpv.service('f04Service', function ($http, $q) {
 		return this.request(config);
 	}
 	
-	this.getTextilePrintings = () =>{
-		return textilePrintings;
+	// TODO poner la ruta en plural
+	this.getTextilePrintings = (pageNumber, pageSize) =>{
+		let config = {
+			method: 'GET',
+			url: urlBase+`/textileprinting/search?page=${pageNumber}&size=${pageSize}`
+		};
+		return this.request(config);
 	}
 
 });
