417ff9d905e74f7d4507331aff4aa43d27cb93b7
Sergio Banegas <sbanegas92@gmail.com>
2017-03-13 19:28:56 +0100

    Controllers IT of search function updated with sort functionality

--- src/test/java/controllers/ArticleControllerIT.java
+++ src/test/java/controllers/ArticleControllerIT.java
@@ -12,6 +12,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -29,7 +30,8 @@ public class ArticleControllerIT {
 
     @Test
     public void testSearchWithoutParameters() {
-        Page<ArticleWrapper> articlePage = articleController.search(new PageRequest(1, 4), null, null, null, null, false);
+        Page<ArticleWrapper> articlePage = articleController.search(new PageRequest(1, 4, Direction.ASC, "retailPrice"), null, null, null,
+                null, false);
         assertNotNull(articlePage);
         assertTrue(articlePage.getNumberOfElements() > 0);
         assertEquals(8, articlePage.getTotalElements());
@@ -43,14 +45,14 @@ public class ArticleControllerIT {
 
     @Test
     public void testSearchArticlesWithParameters() {
-        Page<ArticleWrapper> articlePage = articleController.search(new PageRequest(1, 4), null, "article", new BigDecimal(21),
-                new BigDecimal(26), false);
+        Page<ArticleWrapper> articlePage = articleController.search(new PageRequest(1, 4, Direction.DESC, "retailPrice"), null, "article",
+                new BigDecimal(21), new BigDecimal(26), false);
         assertNotNull(articlePage);
         assertTrue(articlePage.getNumberOfElements() > 0);
         assertEquals(5, articlePage.getTotalElements());
         assertEquals(1, articlePage.getNumber());
         assertTrue(articlePage.hasContent());
-        assertEquals("article6", articlePage.getContent().get(0).getReference());
+        assertEquals("article1", articlePage.getContent().get(0).getReference());
         assertEquals(2, articlePage.getTotalPages());
         assertTrue(articlePage.isLast());
         assertFalse(articlePage.isFirst());
@@ -58,7 +60,8 @@ public class ArticleControllerIT {
 
     @Test
     public void testSearchArticlesOnlyOnStock() {
-        Page<ArticleWrapper> articlePage = articleController.search(new PageRequest(1, 4), null, null, null, null, true);
+        Page<ArticleWrapper> articlePage = articleController.search(new PageRequest(1, 4, Direction.ASC, "retailPrice"), null, null, null,
+                null, true);
         assertNotNull(articlePage);
         assertEquals(0, articlePage.getTotalElements());
         assertFalse(articlePage.hasContent());
