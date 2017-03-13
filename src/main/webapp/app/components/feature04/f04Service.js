4aff9b4d5e334812bdedc62af860119cd9f2ae5c
Sergio Banegas <sbanegas92@gmail.com>
2017-03-13 13:23:10 +0100

    Function to format undefined values added

--- src/main/webapp/app/components/feature04/f04Service.js
+++ src/main/webapp/app/components/feature04/f04Service.js
@@ -23,17 +23,21 @@ tpv.service('f04Service', function ($http, $q) {
 	this.getArticles = (pageNumber, pageSize, reference, description, minRetailPrice, maxRetailPrice, onlyOnStock) =>{
 		  let config = {
 		 	 method: 'GET',
-		 	 url: `${urlBase}/articles/search?page=${pageNumber}&size=${pageSize}&description=${description}` + 
+		 	 url: `${urlBase}/articles/search?page=${pageNumber}&size=${pageSize}&reference=${reference}&description=${description}` + 
 		 	 `&minRetailPrice=${minRetailPrice}&maxRetailPrice=${maxRetailPrice}&onlyOnStock=${onlyOnStock}`
 		  };
 		  console.log(config.url);
 	      return this.request(config);
 	}
    
-	this.getEmbroideries = (pageNumber, pageSize) =>{
+	this.getEmbroideries = (pageNumber, pageSize, reference, description, minRetailPrice, maxRetailPrice, minStitches, maxStitches, 
+			minColors, maxColors, minSquareMillimeters, maxSquareMillimeters) =>{
 		let config = {
 			method: 'GET',
-			url: `${urlBase}/embroideries/search?page=${pageNumber}&size=${pageSize}`
+			url: `${urlBase}/embroideries/search?page=${pageNumber}&size=${pageSize}&reference=${reference}&description=${description}` + 
+		 	 `&minRetailPrice=${minRetailPrice}&maxRetailPrice=${maxRetailPrice}&minStitches=${minStitches}` + 
+		 	 `&maxStitches=${maxStitches}&minColors=${minColors}&maxColors=${maxColors}` + 
+		 	 `&minSquareMillimeters=${minSquareMillimeters}&maxSquareMillimeters=${maxSquareMillimeters}`
 		};
 		return this.request(config);
 	}
@@ -44,6 +48,14 @@ tpv.service('f04Service', function ($http, $q) {
 			url: `${urlBase}/textileprintings/search?page=${pageNumber}&size=${pageSize}`
 		};
 		return this.request(config);
+	}
+	
+	this.formatEmptyNumber = emptyNumber => {
+		if (emptyNumber == undefined){
+			return "";
+		} else {
+			return emptyNumber;
+		}
 	}
 
 });
