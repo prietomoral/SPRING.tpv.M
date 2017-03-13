7f38bfe8682f39899bced37422ad79ce1c3612be
JorgeCoke <cokelas@gmail.com>
2017-03-13 14:19:30 +0100

    XML Config + Working Schedule

--- src/main/java/config/WebInitializer.java
+++ src/main/java/config/WebInitializer.java
@@ -4,6 +4,8 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
+import org.apache.logging.log4j.LogManager;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -13,8 +15,12 @@ import api.Uris;
 public class WebInitializer implements WebApplicationInitializer {
 
     // Se configura el dispatcher
+    @SuppressWarnings("resource")
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
+        
+        new ClassPathXmlApplicationContext("SpringBatchConfig.xml");
+        
         // Se crea un contexto basado en anotaciones
         AnnotationConfigWebApplicationContext annConWebAppCtx = new AnnotationConfigWebApplicationContext();
 
