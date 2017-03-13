99c08564da4f561b4178dccb985a6916258ecc0f
JRSP <JRSP@Javi-AsusA555L>
2017-03-13 09:50:49 +0100

    Create Second method in controller

--- src/main/java/controllers/StatisticController.java
+++ src/main/java/controllers/StatisticController.java
@@ -1,14 +1,17 @@
 package controllers;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import daos.core.ShoppingDao;
+import entities.core.Shopping;
 import services.SemiWrapperStatisticSold;
 import services.StatisticService;
+import wrappers.StatisticProductByDateWrapper;
 import wrappers.TotalSoldProductWrapper;
 
 @Controller
@@ -36,6 +39,16 @@ public class StatisticController {
             mostSoldProducts.add(soldProduct);
         }
         return mostSoldProducts;
+    }
+    
+    public List<StatisticProductByDateWrapper> getSoldsOfProductByDate(long id, Calendar inicio,Calendar fin){
+        List<StatisticProductByDateWrapper> listStatisticProduct=new ArrayList<StatisticProductByDateWrapper>();
+        List<Shopping> listShoppingByDate=shoppingDao.findShoppingsProductByDate(id, inicio, fin);
+        for (Shopping shopping : listShoppingByDate) {
+            StatisticProductByDateWrapper productByDateW=new StatisticProductByDateWrapper(shopping.getId(),shopping.getProductId(),shopping.getDescription(),shopping.getAmount());
+            listStatisticProduct.add(productByDateW);
+        }
+        return listStatisticProduct;
     }
 
 }
