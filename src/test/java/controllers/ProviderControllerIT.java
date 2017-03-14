f60a01e37015733ce37bb8f0bdde2d3b0190f28f
Tania Guzman <taguzga@gmail.com>
2017-03-14 10:58:15 +0100

    Refactoring

--- src/test/java/controllers/ProviderControllerIT.java
+++ src/test/java/controllers/ProviderControllerIT.java
@@ -4,19 +4,15 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
-import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import api.RestService;
 import config.PersistenceConfig;
 import config.TestsControllerConfig;
 import config.TestsPersistenceConfig;
-import entities.core.Provider;
-import wrappers.ProviderAddWrapper;
 import wrappers.ProviderIdCompanyWrapper;
 import wrappers.ProviderWrapper;
 
@@ -38,17 +34,5 @@ public class ProviderControllerIT {
     public void testGetIdCompanyProviders() {
         List<ProviderIdCompanyWrapper> providers = providerController.getAllIdCompany();
         assertEquals(4, providers.size());
-    }  
-	
-	@Test
-	public void testCreateProvider() {
-		Provider p = providerController.add(new ProviderAddWrapper("Company", "Address", 666, 999, "Payment conditions", "Note"));
-		assertEquals("Company", p.getCompany());
-	}
-	
-	@After
-	public void resetData() {
-		new RestService().deleteAll();
-		new RestService().populate();
-	}
+    }
 }
