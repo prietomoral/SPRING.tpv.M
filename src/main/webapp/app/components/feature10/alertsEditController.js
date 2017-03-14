4966db711d8d015df49804d1ecef1f2e92a8c776
Fabricio Flores <fabrifloresg@gmail.com>
2017-03-14 10:36:05 +0100

    Add edit controller

--- src/main/webapp/app/components/feature10/alertsEditController.js
+++ src/main/webapp/app/components/feature10/alertsEditController.js
@@ -1,7 +1,44 @@
 
-tpv.controller('AlertsEditController', AlertsEditController);
+tpv.controller('AlertsEditController',['AlertsService',
+                                       '$routeParams',
+                                       'articles',
+                                       '$location',
+                                       'Alertify',
+                                       AlertsEditController]);
 
-function AlertsEditController() {
+function AlertsEditController(AlertsService, $routeParams, articles, $location, Alertify) {
   'use strict';
   var vm = this;
+  vm.articles = articles;
+  vm.editAlert = editAlert;
+
+  init();
+
+  function init(){
+    AlertsService.getAlert($routeParams.id).then(function success(response){
+      vm.alert = response;
+      vm.alert.article = setSelectedArticle();
+    },
+    function error(errors){
+      console.log(errors);
+    });
+  }
+
+  function editAlert(){
+    vm.alert.product_id = vm.alert.article.originalObject.id;
+    AlertsService.editAlert($routeParams.id, vm.alert).then(function success(response){
+      $location.url('/feature10/' + $routeParams.id + '/show');
+      Alertify.success('Alerta actualizada satisfactoriamente');
+    },
+    function error(errors){
+      Alertify.error(errors.data.description);
+    });
+  }
+
+  function setSelectedArticle(){
+    var article = vm.articles.filter(function(row){
+      return row.id === vm.alert.product_id
+    });
+    return article[0];
+  }
 }
--- src/main/webapp/app/config.js
+++ src/main/webapp/app/config.js
@@ -1,7 +1,7 @@
 var tpv = angular.module("tpv", ["ngRoute",
                                  'bw.paging',
                                  "Alertify",
-                                 "angucomplete",
+                                 "angucomplete-alt",
                                  "ngMessages"]);
 
 tpv.config(function ($routeProvider) {
@@ -140,7 +140,19 @@ tpv.config(function ($routeProvider) {
         .when("/feature10/:id/edit", {
             templateUrl: "app/components/feature10/edit.html",
             controller: "AlertsEditController",
-            controllerAs: "vm"
+            controllerAs: "vm",
+            resolve: {
+              articles: function(f03Service, $location, Alertify){
+                return f03Service.listAllArticles().then(function success(response){
+                  if (response.length === 0) {
+                    $location.url('/feature10');
+                    Alertify.log('No existen artículos.\nNecesitas crear uno antes de crear alertas');
+                  }else{
+                    return response;
+                  }
+                });
+              }
+            }
         })
         .when("/feature15", {
             templateUrl: "app/components/feature15/generatePDF.html",
