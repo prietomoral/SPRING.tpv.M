a55519a8940a516a5c22c1e0e8665a41aeaa0acc
Tania Guzman <taguzga@gmail.com>
2017-03-14 11:30:39 +0100

    Added method api and controller

--- src/main/java/api/ProviderResource.java
+++ src/main/java/api/ProviderResource.java
@@ -3,6 +3,7 @@ package api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -36,5 +37,10 @@ public class ProviderResource {
     @RequestMapping(value = Uris.PROVIDERS_ID_COMPANY, method = RequestMethod.GET)
     public List<ProviderIdCompanyWrapper> getAllIdCompany() {
         return this.providerController.getAllIdCompany();
+    }
+    
+    @RequestMapping(value = Uris.ID, method = RequestMethod.GET)
+    public ProviderWrapper getOneById(@PathVariable(value = "id") int id) {
+        return this.providerController.getOneById(id);
     }
 }
