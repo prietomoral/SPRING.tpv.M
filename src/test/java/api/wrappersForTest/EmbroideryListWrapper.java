e9a66f4bd3f08eb51f25e2a15a5d9c25a75a09fa
Jahir Lozano <jahirlozano@gmail.com>
2017-03-17 21:18:54 +0100

    Refactoring EmbroideryResourceFuntionalTesting

---
+++ src/test/java/api/wrappersForTest/EmbroideryListWrapper.java
@@ -0,0 +1,17 @@
+package api.wrappersForTest;
+
+import java.util.ArrayList;
+
+
+import wrappers.EmbroideryWrapper;
+
+public class EmbroideryListWrapper extends ArrayList<EmbroideryWrapper> {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    public EmbroideryListWrapper() {
+    }
+}
