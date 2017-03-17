4815204254bdea1a5c673f8178edb02e3a65728b
Jahir Lozano <jahirlozano@gmail.com>
2017-03-17 23:29:44 +0100

    Refactoring TextilePrintingResourceFuntionalTesting

---
+++ src/test/java/api/wrappersForTest/TextilePrintingListWrapper.java
@@ -0,0 +1,17 @@
+package api.wrappersForTest;
+
+import java.util.ArrayList;
+
+import wrappers.TextilePrintingWrapper;
+
+public class TextilePrintingListWrapper extends ArrayList<TextilePrintingWrapper> {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    public TextilePrintingListWrapper() {
+    }
+
+}
