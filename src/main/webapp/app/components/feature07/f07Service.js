46d2e218e97876b623b69764c7f66a3b838e7789
marianasalcedo <msalcedo.inf@gmail.com>
2017-03-18 13:16:04 +0100

    Create use voucher Angular module

--- src/main/webapp/app/components/feature07/f07Service.js
+++ src/main/webapp/app/components/feature07/f07Service.js
@@ -21,7 +21,6 @@ tpv.service('f07Service', ['$http', '$q', function ($http, $q) {
       return deferred.promise;   
    }
    
-   
    this.createVoucher = function(value){
 	  let config = {
 	 	      method: 'POST',
@@ -29,6 +28,21 @@ tpv.service('f07Service', ['$http', '$q', function ($http, $q) {
 	      data: value
 	  };
 	  return this.request(config);
+   }
+
+   this.useVoucher = function(value){
+//	  let config = {
+//	 	 method: 'PUT',
+//	     url: urlBase+ "/" + resource + "/use",
+//	     data: {
+//	   	 	identification: value
+//	   	 }
+//	  };
+//		  return this.request(config);
+	      
+	   let deferred = $q.defer();
+	  deferred.resolve("Ok");
+	  return deferred.promise; 
    }
    
    this.getVouchers = function(){
