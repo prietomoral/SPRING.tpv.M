bb58107ba86aa50905f946b71c276b84e698a894
Cori <Cori@localhost>
2017-03-19 21:20:58 +0100

    Revert "Correción de tests"
    This reverts commit 0b22529c7fb18c99b4072f4d78af7e48079f7b9f.

--- src/test/java/api/InvoiceResourceFunctionalTesting.java
+++ src/test/java/api/InvoiceResourceFunctionalTesting.java
@@ -10,45 +10,42 @@ import org.springframework.http.HttpStatus;
 import wrappers.IdTicketWrapper;
 
 public class InvoiceResourceFunctionalTesting {
-	
-    private static String token;
-	    
+    
     @Rule
     public ExpectedException thrown = ExpectedException.none();
     
     @BeforeClass
     public static void populate() {
         new RestService().populate();
-        token = new RestService().loginAdmin();
     }
     
     @Test
     public void testIdTicketNotFound(){
         thrown.expect(new HttpMatcher(HttpStatus.NOT_FOUND));
-        new RestBuilder<Object>(RestService.URL).path(Uris.INVOICES).body(new IdTicketWrapper(10)).basicAuth(token, "").post().build();
+        new RestBuilder<Object>(RestService.URL).path(Uris.INVOICES).body(new IdTicketWrapper(10)).post().build();
     }
     
     @Test
     public void testUserNotFoundInTicket(){
         thrown.expect(new HttpMatcher(HttpStatus.NOT_FOUND));
-        new RestBuilder<Object>(RestService.URL).path(Uris.INVOICES).body(new IdTicketWrapper(1)).basicAuth(token, "").post().build();
+        new RestBuilder<Object>(RestService.URL).path(Uris.INVOICES).body(new IdTicketWrapper(1)).post().build();
     }
     
     @Test
     public void testTicketIsNotClosed(){
         thrown.expect(new HttpMatcher(HttpStatus.CONFLICT));
-        new RestBuilder<Object>(RestService.URL).path(Uris.INVOICES).body(new IdTicketWrapper(2)).basicAuth(token, "").post().build();
+        new RestBuilder<Object>(RestService.URL).path(Uris.INVOICES).body(new IdTicketWrapper(2)).post().build();
     }
     
     @Test
     public void testTicketHasAlreadyInvoice(){
         thrown.expect(new HttpMatcher(HttpStatus.CONFLICT));
-        new RestBuilder<Object>(RestService.URL).path(Uris.INVOICES).body(new IdTicketWrapper(4)).basicAuth(token, "").post().build();
+        new RestBuilder<Object>(RestService.URL).path(Uris.INVOICES).body(new IdTicketWrapper(4)).post().build();
     }
     
     @Test
     public void testCreateInvoice(){
-        new RestBuilder<Object>(RestService.URL).path(Uris.INVOICES).body(new IdTicketWrapper(5)).basicAuth(token, "").post().build();
+        new RestBuilder<Object>(RestService.URL).path(Uris.INVOICES).body(new IdTicketWrapper(5)).post().build();
     }
     
     @AfterClass
